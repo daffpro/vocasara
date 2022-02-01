d3f460b8d9514e16e278619366b144fd5d250b3d
daffpro <53918872+daffpro@users.noreply.github.com>
2022-02-01 03:26:16 +0100

    commit

--- components/Section7.js
+++ components/Section7.js
@@ -6,71 +6,71 @@ const Section7 =() =>{
     
     return(
         <div className='gallery-pad'>
-            <div class="gallery">
-                <div class="gallery__column">
-                    <a href="https://unsplash.com/@marilezhava" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/slider4.jpeg" alt="Portrait by Mari Lezhava" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Equipes dynamiques</figcaption>
+            <div className="gallery">
+                <div className="gallery__column">
+                    <a href="https://unsplash.com/@marilezhava" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/slider4.jpeg" alt="Portrait by Mari Lezhava" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Equipes dynamiques</figcaption>
                         </figure>
                     </a>
-                    <a href="https://unsplash.com/@oladimeg" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/slider5.jpeg" alt="Portrait by Oladimeji Odunsi" class="gallery__image" />
-                            <figcaption class="gallery__caption">Spécialistes en énergie</figcaption>
+                    <a href="https://unsplash.com/@oladimeg" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/slider5.jpeg" alt="Portrait by Oladimeji Odunsi" className="gallery__image" />
+                            <figcaption className="gallery__caption">Spécialistes en énergie</figcaption>
                         </figure>
                     </a>
                 </div>
                 
-                <div class="gallery__column">                    
-                    <a href="https://unsplash.com/@von_co" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/slider3 (2).jpeg" alt="Portrait by Ivana Cajina" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Toujours plus proche de vous</figcaption>
+                <div className="gallery__column">                    
+                    <a href="https://unsplash.com/@von_co" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/slider3 (2).jpeg" alt="Portrait by Ivana Cajina" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Toujours plus proche de vous</figcaption>
                         </figure>
                     </a>
-                    <a href="https://unsplash.com/@samburriss" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/silder10.jpeg" alt="Portrait by Sam Burriss" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Investis dans l'éducation</figcaption>
+                    <a href="https://unsplash.com/@samburriss" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/silder10.jpeg" alt="Portrait by Sam Burriss" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Investis dans l'éducation</figcaption>
                         </figure>
                     </a>  
                 </div>
                 
-                <div class="gallery__column">
-                    <a href="https://unsplash.com/@marilezhava" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/slider9.jpeg" alt="Portrait by Mari Lezhava" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Couverture médiatique</figcaption>
+                <div className="gallery__column">
+                    <a href="https://unsplash.com/@marilezhava" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/slider9.jpeg" alt="Portrait by Mari Lezhava" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Couverture médiatique</figcaption>
                         </figure>
                     </a>
                     
-                    <a href="https://unsplash.com/@samburriss" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/slider8.jpeg" alt="Portrait by Sam Burriss" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Dynamisme sur le terrain</figcaption>
+                    <a href="https://unsplash.com/@samburriss" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/slider8.jpeg" alt="Portrait by Sam Burriss" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Dynamisme sur le terrain</figcaption>
                         </figure>
                     </a>
-                    <a href="https://unsplash.com/@samburriss" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/drone-g4dcd1fa1d_1920.jpg" alt="Portrait by Sam Burriss" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Voler plus proche de vous</figcaption>
+                    <a href="https://unsplash.com/@samburriss" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/drone-g4dcd1fa1d_1920.jpg" alt="Portrait by Sam Burriss" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Voler plus proche de vous</figcaption>
                         </figure>
                     </a>
                 </div>
                 
-                <div class="gallery__column">
-                    <a href="https://unsplash.com/@frxgui" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/slider2.jpeg" alt="Portrait by Guilian Fremaux" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Equipements de qualités</figcaption>
+                <div className="gallery__column">
+                    <a href="https://unsplash.com/@frxgui" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/slider2.jpeg" alt="Portrait by Guilian Fremaux" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Equipements de qualités</figcaption>
                         </figure>
                     </a>
 
-                    <a href="https://unsplash.com/@majestical_jasmin" target="_blank" class="gallery__link">
-                        <figure class="gallery__thumb">
-                            <img src="/img/slider11.jpeg" alt="Portrait by Jasmin Chew" class="gallery__image"/>
-                            <figcaption class="gallery__caption">Acharnement dans le travail</figcaption>
+                    <a href="https://unsplash.com/@majestical_jasmin" target="_blank" className="gallery__link">
+                        <figure className="gallery__thumb">
+                            <img src="/img/slider11.jpeg" alt="Portrait by Jasmin Chew" className="gallery__image"/>
+                            <figcaption className="gallery__caption">Acharnement dans le travail</figcaption>
                         </figure>
                     </a>
                 </div>
